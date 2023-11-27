5271487f6b446a6c6aaf371c606528386cbba276
Sukanta Das <111263085+Sukantadas1@users.noreply.github.com>
2023-11-27 11:23:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+ function handleInput(input) {
+            const nextInput = input.nextElementSibling;
+            const prevInput = input.previousElementSibling;
+
+            if (input.value !== '') {
+                if (nextInput) {
+                    nextInput.focus();
+                }
+            } else {
+                if (prevInput) {
+                    prevInput.focus();
+                }
+            }
+        }
